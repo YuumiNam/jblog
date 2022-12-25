3c2c8deba6142fc549941b666a5d31d4a4e133b0
이진무 <@211.226.78.213>
2022-12-25 16:03:55 +0900

    [jblog #09] Category의 Post수가 0일때 카테고리가 안나오던 현상 쿼리 수정

--- sql/category.sql
+++ sql/category.sql
@@ -1,4 +1,5 @@
 select * from category;
+select * from post;
 
 -- 카테고리 생성
 insert into category values (null, '카테고리10', '카테고리10', 'dltjsam');
@@ -10,6 +11,17 @@ select a.no, a.title, a.expla, b.postCount
 						group by category_no) b
     where a.no = b.category_no
 		and id = 'dltjsam';
+
+-- findAll 포스트 수가 0일때에도 카테고리가 온전히 잘 나옴
+select (select count(*) from post p
+				where category_no=c.no) num 
+			from category c 
+			where id='dltjsam';
+
+-- findAll 포스트 수가 0인 카테고리는 안나옴 (오답)
+select category_no, count(*) as postCount
+						from post
+						group by category_no;
 
 -- delete
 delete 
